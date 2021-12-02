e9f59438ad531b5fa036f1d7877a22af39aab1a3
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-12-02 00:14:43 -0300

    Using MPU6050_Solar class instead of a generic one to read the sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
-#include <I2Cyangui.h>
 #include <Kalman.h>  // Source: https://github.com/TKJElectronics/KalmanFilter
 #include <RtcDS3231.h>
 #include <RtcDateTime.h>
 #include <Time.h>
 #include <Wire.h>
 #include <analogWrite.h>
+#include <math.h>
+
+#include <MPU/MPU.hpp>
 #include <motor/motor.hpp>
 
 //----------------------------RTC settings----------------------------------------//
@@ -19,8 +21,9 @@ Kalman kalmanX;  //Criação de objeto
 Kalman kalmanY;  //Criação de objeto
 
 //----------------------------IMU settings---------------------------------------//
-double accX, accY, accZ, gyroX, gyroY, gyroZ, gyroXangle, gyroYangle;
-int16_t tempRaw;
+MPUData MPU_Data;
+MPU6050_Solar mpu(0x69, MPU_Data);
+double gyroXangle, gyroYangle;
 double compAngleX, compAngleY;  // Calculated angle using a complementary filter
 double kalAngleX, kalAngleY;    // Calculated angle using a Kalman filter
 uint32_t timer;
@@ -34,8 +37,6 @@ float const LATITUDE = -22.8613427;
 int const TIMEZONE = -3;
 SunLight Sun_Time;
 
-//----------------------------I2c Comunication---------------------------------------//
-comunication com;
 //----------------------------Driver Settings---------------------------------------//
 
 #define LPWM 4     //lpwm
@@ -75,39 +76,15 @@ void setup() {
     rtc.Begin();                //Inicialização do RTC DS3231
     rtc.SetDateTime(RTC_Data);  //Configurando valores iniciais do RTC DS3231
 
-    i2cData[0] = 7;     // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
-    i2cData[1] = 0x00;  // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
-    i2cData[2] = 0x00;  // Set Gyro Full Scale Range to ±250deg/s
-    i2cData[3] = 0x00;  // Set Accelerometer Full Scale Range to ±2g
+    //---------------------------------MPU settings-----------------------------//
+    mpu.init();     // Configura e inicia o MPU
+    mpu.readMPU();  // realiza a primeira leitura do MPU para preencher os dados do MPUData
 
-    while (com.i2cWritey(0x19, i2cData, 4, false))  // Write to all four registers at once
-        ;
-    dataI2c = 0x01;
-    while (com.i2cWritey(0x6B, &dataI2c, 1, true))  // PLL with X axis gyroscope reference and disable sleep mode
-        ;
-
-    while (com.i2cRead(0x75, i2cData, 1, true))
-        ;
-    if (i2cData[0] != 0x68) {  // Read "WHO_AM_I" register //0x69 for AD0 High default of solar sensor project
-        Serial.print(F("Error reading sensor"));
-        while (1)
-            ;
-    }
-
-    delay(100);  // Wait for sensor to stabilize
-
-    //---------------------------kalman----------------------------------//
-    // Set kalman and gyro starting angle //
-    while (com.i2cRead(0x3B, i2cData, 6, true))
-        ;
-    accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
-    accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
-    accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
-
+    //---------------------------Kalman----------------------------------//
 // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
-#ifdef RESTRICT_PITCH                              // Eq. 25 and 26
-    double roll = atan2(accY, accZ) * RAD_TO_DEG;  //atan2 outputs the value of -π to π (radians. It is then converted from radians to degrees
-    double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#ifdef RESTRICT_PITCH                                              // Eq. 25 and 26
+    double roll = atan2(MPU_Data.AcY, MPU_Data.AcZ) * RAD_TO_DEG;  //atan2 outputs the value of -π to π (radians. It is then converted from radians to degrees
+    double pitch = atan(-MPU_Data.AcX / sqrt(pow(MPU_Data.AcY, 2) + pow(MPU_Data.AcZ, 2))) * RAD_TO_DEG;
 #else  // Eq. 28 and 29
     double roll = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
     double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
@@ -142,6 +119,19 @@ void callRTC(RtcDateTime &RTC_Data) {
     Serial.print(":");
     Serial.print(RTC_Data.Second());  //Imprimindo o Segundo
     Serial.print("  ");
+#endif
+}
+
+void callMPU() {
+    mpu.readMPU();
+
+#ifdef DEBUG_MPU
+    Serial.print(" | X= ");
+    Serial.print(MPU_Data.AcX);
+    Serial.print(" | Y= ");
+    Serial.print(MPU_Data.AcY);
+    Serial.print(" | Z= ");
+    Serial.print(MPU_Data.AcZ);
 #endif
 }
 
@@ -201,26 +191,13 @@ void Erro_Read(int Setpoint, int Input) {
 }
 
 void loop() {
-    callRTC(RTC_Data);  //Atualiza RTC
-    //callMPU(MPU_Data);  // Atualiza MPU
+    callRTC(RTC_Data);  // Atualiza RTC
+    callMPU();          // Atualiza MPU
 
     // Calculating Sun parameters  //
 
     Sun_Time.Sun_Range(LONGITUDE, LATITUDE, TIMEZONE);
     double Sun_Setpoint = Sun_Time.Sun_Position(RTC_Data.Second(), RTC_Data.Minute(), RTC_Data.Hour(), RTC_Data.Day(), RTC_Data.Month(), RTC_Data.Year());
-
-    //---------------------------------MPU_Read-------------------------------------//
-
-    while (com.i2cRead(0x3B, i2cData, 14, true))
-        ;  // Update all the values //
-    accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
-    accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
-    accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
-    tempRaw = (int16_t)((i2cData[6] << 8) | i2cData[7]);
-    gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
-    gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
-    gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);
-    ;
 
     double dt = (double)(micros() - timer) / 1000000;  // Calculate delta time
     timer = micros();
@@ -229,16 +206,16 @@ void loop() {
     // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
     // It is then converted from radians to degrees
 
-#ifdef RESTRICT_PITCH  // Eq. 25 and 26
-    double roll = atan2(accY, accZ) * RAD_TO_DEG;
-    double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#ifdef RESTRICT_PITCH                                              // Eq. 25 and 26
+    double roll = atan2(MPU_Data.AcY, MPU_Data.AcZ) * RAD_TO_DEG;  //atan2 outputs the value of -π to π (radians. It is then converted from radians to degrees
+    double pitch = atan(-MPU_Data.AcX / sqrt(pow(MPU_Data.AcY, 2) + pow(MPU_Data.AcZ, 2))) * RAD_TO_DEG;
 #else  // Eq. 28 and 29
     double roll = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
     double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
 #endif
 
-    double gyroXrate = gyroX / 131.0;  // Convert to deg/s
-    double gyroYrate = gyroY / 131.0;  // Convert to deg/s
+    double gyroXrate = MPU_Data.GyX / 131.0;  // Convert to deg/s
+    double gyroYrate = MPU_Data.GyY / 131.0;  // Convert to deg/s
 
 #ifdef RESTRICT_PITCH
     // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
