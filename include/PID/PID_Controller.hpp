f53c910015a6c46139da9227ecf4fbeb7ac3cbb0
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2021-12-09 16:47:25 -0300

    fixing issues with PID_Controller class & adding auto reset of I value

--- include/PID/PID_Controller.hpp
+++ include/PID/PID_Controller.hpp
@@ -12,9 +12,10 @@ class PID_Controller {
     int _outputLimit;
     int _integrativeLimit;
     double _threshold;
+    int _stabilityCounter = 0;
 
    public:
-    PID_Controller(double kp, double ki, double kd, double threshold = 1.7, int outputLimit = 255, int integrativeLimit = 200) {
+    PID_Controller(double kp, double ki, double kd, double threshold = 1.7, int outputLimit = 255, int integrativeLimit = 35) {
         _kp = kp;
         _ki = ki;
         _kd = kd;
@@ -23,25 +24,41 @@ class PID_Controller {
         _integrativeLimit = integrativeLimit;
     }
 
-    int calculateOutput(double error, unsigned long time = millis()) {
+    int calculateOutput(double error, double target = 0, unsigned long time = millis()) {
+        error -= target;
+
         if (abs(error) < _threshold) {
-            this->reset();
+            _stabilityCounter++;
+            if (_stabilityCounter > 250) {
+                this->reset();
+                _stabilityCounter = 0;  // Caso um RESET na constante integrativa seja necessário, descomentar
+            }
+            _lastRun = time;
+            _lastError = 0.;
             return 0;
         }
+        _stabilityCounter = 0;
 
         int dt = time - _lastRun;
-        _lastRun = time;
-
         double P = _kp * error;
         double I = _lastIntegrativeValue + (_ki * error * dt / 1000.0);
-        I = constrain(I, -_integrativeLimit, _integrativeLimit);
         double D = _kd * ((error - _lastError) / dt);
+        I = constrain(I, -_integrativeLimit, _integrativeLimit);
         //error = (KFE * error) + ((1 - KFE) * lastError);  //Filtra a variacao do erro para a derivada
 
+        _lastRun = time;
         _lastIntegrativeValue = I;
+        _lastError = error;
 
         int output = static_cast<int>(P + I + D);
-
+#ifdef DEBUG_PID
+        Serial.print(" | P: ");
+        Serial.printf("%5.3f", P);
+        Serial.print(" | I: ");
+        Serial.printf("%5.3f", I);
+        Serial.print(" | D: ");
+        Serial.printf("%5.3f", D);
+#endif
         return constrain(output, -_outputLimit, _outputLimit);
     }
 
