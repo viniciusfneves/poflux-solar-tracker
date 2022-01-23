5754eab6fe8e61ffac2450019f28e51f965d3838
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2022-01-23 09:57:29 -0300

    serving html on webServer. User Authentication necessary. SPIFFS

--- include/HTTPServer/http_server.hpp
+++ include/HTTPServer/http_server.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+#define AUTH_USER "user"
+#define AUTH_PASS "admin"
 
 AsyncWebServer server(80);
 
 void initHTTPServer() {
-    server.on("/", [](AsyncWebServerRequest *request) { request->send(200, "text/plain", "POF LUX WEBSERVER ONLINE!"); });
+    SPIFFS.begin();
+
+    server.on("/", [](AsyncWebServerRequest *request) { request->redirect("/pof-lux"); });
+
+    server.serveStatic("/pof-lux", SPIFFS, "/index.html").setAuthentication(AUTH_USER, AUTH_PASS);
 
     server.onNotFound([](AsyncWebServerRequest *request) { request->send(404, "text/plain", "NOT FOUND"); });
 
