6cfa597acc7d525027be97ea87cbfffaad99b1c6
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2022-02-09 00:00:21 -0300

    Corrigindo algumas lógicas da classe PID + adição de dados recuperáveis

--- include/PID/PID_Controller.hpp
+++ include/PID/PID_Controller.hpp
@@ -7,7 +7,8 @@
 class PID_Controller {
    private:
     double _kp, _ki, _kd;
-    int _outputLimit;
+    double _p, _i, _d, _output;
+    int _outputLimit = 100;
     int _integrativeLimit;
     double _integrativeLimitPercentage;
     double _threshold;
@@ -36,24 +37,21 @@ class PID_Controller {
         _kp = kp;
         _ki = ki;
         _kd = kd;
-        _setBoundaries();
+        _integrativeLimit = (int)(_outputLimit * _integrativeLimitPercentage);
     }
 
-    void setKp(double kp) {
-        _kp = kp;
-        _setBoundaries();
-    }
-
-    void setKi(double ki) {
-        _ki = ki;
-        _setBoundaries();
-    }
-
+    void setKp(double kp) { _kp = kp; }
+    void setKi(double ki) { _ki = ki; }
     void setKd(double kd) { _kd = kd; }
 
     double getKp() { return _kp; }
     double getKi() { return _ki; }
     double getKd() { return _kd; }
+
+    double getInstantP() { return _p; }
+    double getInstantI() { return _i; }
+    double getInstantD() { return _d; }
+    int getOutput() { return (int)_output; }
 
     int calculateOutput(double actualState, double target = 0., unsigned long time = millis()) {
         double error = actualState - target;
@@ -62,38 +60,36 @@ class PID_Controller {
         if (abs(error) < _threshold) {
             if (_lastUnstableTimestamp + TIME_T0_STABILIZE > time)
                 this->reset();
-
-            double D = _kd * -_lastError / dt;
-#ifdef DEBUG_PID
-            _debugPrints(actualState, target, 0., _lastIntegrativeValue, D, 0);
-#endif
+            _p = 0;
+            _d = _kd * -_lastError / dt;
             _lastRun = time;
             _lastError = 0.;
+            _output = 0;
             return 0;
         }
 
-        double P = _kp * error;
-        double I = _lastIntegrativeValue + (_ki * error * dt / 1000.0);
-        double D = _kd * (error - _lastError) / dt;
-        I = constrain(I, -_integrativeLimit, _integrativeLimit);
+        _p = _kp * error;
+        _i = _lastIntegrativeValue + (_ki * error * dt / 1000.0);
+        _d = _kd * (error - _lastError) / dt;
+        _i = constrain(_i, -_integrativeLimit, _integrativeLimit);
         //error = (KFE * error) + ((1 - KFE) * lastError);  //Filtra a variacao do erro para a derivada
 
         _lastUnstableTimestamp = time;
         _lastRun = time;
-        _lastIntegrativeValue = I;
+        _lastIntegrativeValue = _i;
         _lastError = error;
 
-        int output = (int)(P + I + D);
-        output = constrain(output, -_outputLimit, _outputLimit);
-        output = map(output, -_outputLimit, _outputLimit, -255, 255);
+        _output = _p + _i + _d;
+        _output = constrain(_output, -_outputLimit, _outputLimit);
+        _output = map(_output, -_outputLimit, _outputLimit, -255, 255);
 
-#ifdef DEBUG_PID
-        _debugPrints(actualState, target, P, I, D, output);
-#endif
-        return output;
+        return (int)_output;
     }
 
-    void reset() { _lastIntegrativeValue = 0.; }
+    void reset() {
+        _lastIntegrativeValue = 0.;
+        _i = 0;
+    }
 };
 
 PID_Controller pid(1.7, 1, 450);
