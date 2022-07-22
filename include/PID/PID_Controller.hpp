fb4064fb018029c61ab762081fcb9f9333b6b11a
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2022-07-22 14:00:22 -0300

    changing PID threshold to 1.7 degrees

--- include/PID/PID_Controller.hpp
+++ include/PID/PID_Controller.hpp
@@ -20,7 +20,7 @@ class PID_Controller {
    public:
     // O limite integrativo controlado pela variável integrativeLimit deve ser passado em porcentagem
     // Esse valor definirá qual porcentagem máxima de participação no output do controlador a constrante integrativa terá
-    PID_Controller(double kp, double ki, double kd, double threshold = 1.5, int integrativeLimitPercentage = 75) {
+    PID_Controller(double kp, double ki, double kd, double threshold = 1.7, int integrativeLimitPercentage = 75) {
         _integrativeLimitPercentage = (double)integrativeLimitPercentage / 100.;
         _threshold                  = threshold;
         _kp                         = kp;
