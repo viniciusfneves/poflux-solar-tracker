5b1623b391b998e54d8330fa05f542ac31ddc292
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2022-08-11 01:35:40 -0300

    connecting to websocket server direct from static IP

--- data/scripts/websocket.js
+++ data/scripts/websocket.js
@@ -1,5 +1,5 @@
 /*Abre a conexão com serviço WebSocket do ESP*/
-export var ws = new WebSocket("ws://lif.local:81");
+export var ws = new WebSocket("ws://192.168.1.101:81");
 
 ws.onopen = function () {
 	document.getElementById("connection_status").innerHTML = "Status: Conectado";
