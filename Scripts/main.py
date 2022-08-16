68a52ec347ed401f0505f89c8e5b261a9f3bd8a1
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2022-08-16 13:28:20 -0300

    uploading tracking files via FTP and saving them to a folder
    Only working modification in configs and websocket. Main use must be in Time Controller.

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,4 @@
+from ftplib import FTP
 from time import sleep
 import requests
 import wget
@@ -5,11 +6,18 @@ from datetime import datetime
 
 now = datetime.now()
 
-filename = f"POFLUX_{now.strftime('%y%m%d_%H%M%S')}"
+filepath = "./tracking files/"
+filename = f"POFLUX_{now.strftime('%y%m%d_%H%M%S')}.csv"
 
 print(filename)
-wget.download("http://192.168.1.101/pof-lux/tracking", filename)
+wget.download("http://192.168.1.101/pof-lux/tracking", filepath + filename)
 
-sleep(5)
+sleep(15)
 
 x = requests.get("http://192.168.1.101/pof-lux/clear_tracking")
+
+ftp = FTP("146.164.132.215")
+ftp.login("poflux", "hDCsGW&BNBz%K3K")
+with open(filepath + filename, "rb") as datafile:
+    responseMessage = ftp.storbinary('STOR '+ filename, datafile) 
+    print(responseMessage)
