2f6f8dbac9f3edfd83cd8dea1cb3faa5261290fe
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2022-11-23 11:22:37 -0300

    Melhorando alterações no Script de atualização do lif remoto

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,18 +6,20 @@ from datetime import datetime
 
 now = datetime.now()
 
+address = "192.168.10.101"
+
 filepath = "./tracking files/"
 filename = f"POFLUX_{now.strftime('%y%m%d_%H%M%S')}.csv"
 
 print(filename)
-wget.download("http://192.168.1.101/pof-lux/tracking", filepath + filename)
+wget.download(f"http://{address}/pof-lux/tracking", filepath + filename)
 
 sleep(15)
 
-x = requests.get("http://192.168.1.101/pof-lux/clear_tracking")
+x = requests.get(f"http://{address}/pof-lux/clear_tracking")
 
 ftp = FTP("146.164.132.215")
 ftp.login("poflux", "hDCsGW&BNBz%K3K")
 with open(filepath + filename, "rb") as datafile:
-    responseMessage = ftp.storbinary('STOR '+ filename, datafile) 
+    responseMessage = ftp.storbinary('STOR ' + filename, datafile)
     print(responseMessage)
