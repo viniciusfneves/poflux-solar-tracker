d2310a077edf53e24ebfe43549f09ba1921351a9
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2023-04-18 15:36:39 -0300

    Removendo interface de monitoramento | Migrando para um servidor fastAPI
    Creating a new repo for the monitoring interface...

--- data/scripts/websocket.js
+++
@@ -1,6 +0,0 @@
-/*Abre a conexão com serviço WebSocket do ESP*/
-export var ws = new WebSocket("ws://" + window.location.hostname + ":81");
-
-ws.onopen = function () {
-	// document.getElementById("connection_status").innerHTML = "Status: Conectado";
-};
