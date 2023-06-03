571c5effeb6d2ba5fc02469c52e24d4347856775
Vinícius Ferreira Neves <viniciusfneves@poli.ufrj.br>
2023-06-03 17:04:34 -0300

    Implementing http endpoint for configurations with json

--- include/WiFi/HTTPServer/http_server.hpp
+++ include/WiFi/HTTPServer/http_server.hpp
@@ -18,9 +18,22 @@ void startHTTPServer() {
                   request->send(200, "text/plain", "DONE");
               });
 
-    server.on("/config", HTTP_PATCH, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/plain", "CONFIG ENDPOINT...");
-    });
+    server.on(
+        "/config", HTTP_PATCH,
+        [](AsyncWebServerRequest *request) {
+            request->send(200, "text/plain", "CONFIG ENDPOINT...");
+        },
+        NULL,
+        [](AsyncWebServerRequest *request, uint8_t *data, size_t len,
+           size_t index, size_t total) {
+            DynamicJsonDocument  body(1024);
+            DeserializationError error =
+                deserializeJson(body, (const char *)data);
+            if (error) request->send(400, "text/plain", "BAD REQUEST");
+            serializeJsonPretty(body, Serial);
+            request->send(200, "text/plain",
+                          "JSON decoded and printed on the Serial port");
+        });
 
     server.onNotFound([](AsyncWebServerRequest *request) {
         request->send(404, "text/plain", "NOT FOUND");
--- include/configurations/configurations.hpp
+++ include/configurations/configurations.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <ArduinoJson.h>
+
+#include <MPU/MPU.hpp>
+#include <PID/PID_Controller.hpp>
+#include <TimeController/TimeController.hpp>
 #include <motor/motor.hpp>
 
 enum class Mode { Auto, Manual, Halt };
@@ -17,6 +22,55 @@ struct Configs {
         // imediatamente enviado aos motores, não necessitando da execução de
         // outras funções no código
         if (_mode == Mode::Halt) motor.command(0);
+    }
+
+    void configWithJSON(String message) {
+        StaticJsonDocument<1024> jsonM;
+
+        DeserializationError err = deserializeJson(jsonM, message);
+        if (err) return;
+
+        if (jsonM.containsKey("command")) {
+            if (strcmp(jsonM["command"], "reset") == 0) {
+                ESP.restart();
+            }
+        }
+
+        if (jsonM.containsKey("mode")) {
+            if (strcmp(jsonM["mode"], "auto") == 0) {
+                changeMode(Mode::Auto);
+            }
+            if (strcmp(jsonM["mode"], "manual") == 0) {
+                changeMode(Mode::Manual);
+            }
+            if (strcmp(jsonM["mode"], "halt") == 0) {
+                changeMode(Mode::Halt);
+            }
+        }
+
+        if (jsonM.containsKey("manual_setpoint")) {
+            manualSetpoint = jsonM["manual_setpoint"].as<int8_t>();
+        }
+
+        if (jsonM.containsKey("adjust")) {
+            if (jsonM["adjust"].containsKey("kp")) {
+                pid.setKp(jsonM["adjust"]["kp"].as<double>());
+            }
+            if (jsonM["adjust"].containsKey("ki")) {
+                pid.setKi(jsonM["adjust"]["ki"].as<double>());
+            }
+            if (jsonM["adjust"].containsKey("kd")) {
+                pid.setKd(jsonM["adjust"]["kd"].as<double>());
+            }
+            if (jsonM["adjust"].containsKey("error_threshold")) {
+                pid.setThreshold(
+                    jsonM["adjust"]["error_threshold"].as<double>());
+            }
+            if (jsonM["adjust"].containsKey("rtc")) {
+                // {'adjust':{'rtc':Epoch64Time}}
+                timeInfo.setDatetime(jsonM["adjust"]["rtc"].as<int64_t>());
+            }
+        }
     }
 };
 
